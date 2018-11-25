1be379a8d29733497ebe057f95b5455fc4e50e02
Lance Norskog <lance.norskog@walmartlabs.com>
2018-11-24 17:37:53 -0800

    Syllable model and tree work

--- py/SyllableTree.py
+++ py/SyllableTree.py
@@ -8,6 +8,12 @@ class Node:
         self.children={} # all child elements beginning with current syllable
         self.word=None
         
+    def __str__(self):
+        s=''
+        for (k,_) in self.children.items():
+            s+=k + ','
+        return 'word: '+str(self.word)+'; children: '+s
+
 class SyllableTree:
     "syllable->word trie"
     def __init__(self):
@@ -76,11 +82,27 @@ class SyllableTree:
                 
         return words
                 
+    def dump(self):
+        nodes=[self.root]
+        while len(nodes)>0:
+            # put all children into list
+            for _,v in nodes[0].children.items():
+                nodes.append(v)
+            
+            # dump current node
+            print(nodes[0])
+                
+            # remove from list
+            del nodes[0]
+
                 
 if __name__=='__main__':
     t=SyllableTree() # create tree
-    t.addWord('the', ['DH', 'AH']) # add words
-    t.addWord('the', ['DH', 'AE']) # add words
-    t.addWord('them', ['DH', 'AE', 'M']) # add words
+    t.addWord('the', ['DH AH']) # add words
+    t.addWord('the', ['DH AE']) # add words
+    t.addWord('them', ['DH EH M']) # add words
+    t.addWord('themselves', ['DH EH M', 'S EH L VS']) # add words
     print(t.getNextSyllables(['DH'])) # chars following 'th'
-    print(t.getNextWords(['DH', 'AE'])) # all words of which 'th' is prefix
+    print(t.getNextWords(['DH AE'])) # all words of which 'th' is prefix
+    print(t.getNextWords(['DH EH M'])) # all words of which 'th' is prefix
+    t.dump()
